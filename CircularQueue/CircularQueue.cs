1f4f26f2496cd3a0287dd08c0e87de4dca9117cd
Reza Bagheri <bagherbagherreza@gmail.com>
2022-08-23 17:37:15 +0430

    Initial Repo

---
+++ CircularQueue/CircularQueue.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircularQueueWithArray
+{
+    public class CircularQueue
+    {
+        public int[] queue;
+        public int size;
+        public int front;
+        public int rear;
+
+        public CircularQueue(int sizeOfQueue)
+        {
+            size = sizeOfQueue;
+            queue = new int[sizeOfQueue];
+            front = -1;
+            rear = -1;
+        }
+
+        public void Enqueue(int item)
+        {
+            if (IsFull())
+            {
+                Console.WriteLine("The queue is full");
+            }
+            else
+            {
+                if (front == -1)
+                {
+                    front++;
+                }
+
+                rear = (rear + 1) % size;
+                queue[rear] = item;
+
+            }
+        }
+
+        public void Dequeue()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("The queue is empty!");
+            }
+            else
+            {
+                Console.WriteLine(queue[front]);
+
+                if (front == rear)
+                {
+                    front = -1;
+                    rear = -1;
+                }
+                else
+                {
+                    front = (front + 1) % size;
+                }
+            }
+        }
+
+        public void Print()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("The queue is empty!");
+            }
+            else
+            {
+
+                if (rear >= front)
+                {
+                    for (int i = front; i <= rear; i++)
+                    {
+                        Console.WriteLine(queue[i]);
+                    }
+                }
+                else
+                {
+                    for (int i = front; i <= (queue.Length - 1); i++)
+                    {
+                        Console.WriteLine(queue[i]);
+                    }
+                    for (int i = 0; i <= rear; i++)
+                    {
+                        Console.WriteLine(queue[i]);
+                    }
+                }
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            if ((front == -1 && rear == -1) || front > rear)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool IsFull()
+        {
+            if (((rear + 1) == size && front == 0) || (front - 1) == rear)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ CircularQueue/Program.cs
@@ -0,0 +1,21 @@
+﻿using CircularQueueWithArray;
+
+var queue = new CircularQueue(10);
+
+queue.Enqueue(9);
+queue.Enqueue(87);
+
+queue.Dequeue();
+queue.Dequeue();
+queue.Dequeue();
+
+queue.Enqueue(87);
+
+queue.Dequeue();
+
+
+
+
+Console.WriteLine("-------------------------------------");
+
+queue.Print();
